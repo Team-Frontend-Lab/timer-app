c2b89a4dd5bddf974a6efb07b6373bb1c98328d9
Amit Rai <42946110+Amitrai011@users.noreply.github.com>
2025-08-11 19:31:40 +0530

    Add Footer component and update app title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        Challenge by{" "}
+        <a
+          href="https://www.frontendlab.io/projects"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Frontend Lab
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
